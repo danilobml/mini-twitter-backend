1dc93f8aec55e6e3b2522c93c110643566e01f35
danilobml <danbarml@gmail.com>
2022-07-14 23:11:02 +0200

    for deployment

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
-// var helmet = require("helmet");
+var helmet = require("helmet");
+const cors = require("cors");
 
 var messagesRouter = require("./routes/messagesRouter");
 var usersRouter = require("./routes/usersRouter");
@@ -17,11 +18,12 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 
 app.use(logger("dev"));
-// app.use(helmet());
+app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/messages", messagesRouter);
 app.use("/users", usersRouter);
